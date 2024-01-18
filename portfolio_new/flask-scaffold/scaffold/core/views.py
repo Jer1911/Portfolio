44388b255acca03f12ea3f98b5659fcee96e59e0
Jer1911 <eckerjeremy@gmail.com>
2024-01-18 15:14:57 -0600

    Updated flask-scaffold.

--- portfolio_new/flask-scaffold/scaffold/core/views.py
+++ portfolio_new/flask-scaffold/scaffold/core/views.py
@@ -2,6 +2,7 @@ import logging
 
 from flask import render_template, Blueprint, url_for, redirect
 from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.exceptions import HTTPException
 
 from scaffold import db
 from scaffold.models import User
@@ -36,8 +37,21 @@ def login():
         
     return render_template('login.html', form=form)
 
-@login_required
 @core.route('/welcome')
+@login_required
 def welcome():
     username = current_user.username
     return render_template('welcome.html', username=username)
+
+@core.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('core.index'))
+
+@core.app_errorhandler(HTTPException)
+def error(e):
+    """
+    Catchall for HTTPExceptions; shows the custom error page with the code.
+    """
+    return render_template('error.html', code=e.code)
